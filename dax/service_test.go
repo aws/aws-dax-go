e2397b62097ed433c859faee90dd1af305a4a2d9
shamaton <shamaton77@gmail.com>
2023-05-18 23:05:05 +0900

    fix service_test

--- dax/service_test.go
+++ dax/service_test.go
@@ -3,7 +3,7 @@ package dax
 import (
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go-v2/aws"
 )
 
 func TestConfigMergeFrom(t *testing.T) {
@@ -24,22 +24,20 @@ func TestConfigMergeFrom(t *testing.T) {
 		{
 			testName:             "DefaultConfig merging with an aws config that specifies aws.UseServiceDefaultRetries should result in using default retries",
 			daxConfig:            DefaultConfig(),
-			awsConfig:            aws.Config{MaxRetries: aws.Int(aws.UseServiceDefaultRetries)},
+			awsConfig:            aws.Config{RetryMaxAttempts: -1},
 			expectedWriteRetries: 2,
 			expectedReadRetries:  2,
 		},
 		{
 			testName:             "DefaultConfig merging with an aws config that specifies a non-negative MaxRetry should result in using that value as both WriteRetries and ReadRetries",
 			daxConfig:            DefaultConfig(),
-			awsConfig:            aws.Config{MaxRetries: aws.Int(123)},
+			awsConfig:            aws.Config{RetryMaxAttempts: 123},
 			expectedWriteRetries: 123,
 			expectedReadRetries:  123,
 		},
 	}
 
 	for _, testCase := range testCases {
-		testCase := testCase
-
 		t.Run(testCase.testName, func(t *testing.T) {
 			testCase.daxConfig.mergeFrom(testCase.awsConfig)
 			if testCase.daxConfig.WriteRetries != testCase.expectedWriteRetries {
