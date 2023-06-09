99cf40a163f4afe21f928525a1ec4bb6e6783740
shamaton <shamaton77@gmail.com>
2023-06-09 23:05:37 +0900

    delete dax original retryer

--- dax/internal/client/dax_retryer.go
+++ dax/internal/client/dax_retryer.go
@@ -24,63 +24,6 @@ import (
 	"github.com/aws/smithy-go"
 )
 
-////DaxRetryer implements EqualJitterBackoffStratergy for throttled requests
-//type DaxRetryer struct {
-//	BaseThrottleDelay time.Duration
-//	MaxBackoffDelay   time.Duration
-//}
-//
-//const (
-//	//DefaultBaseRetryDelay is base delay for throttled requests
-//	DefaultBaseRetryDelay = 70 * time.Millisecond
-//	//DefaultMaxBackoffDelay is max backoff delay for throttled requests
-//	DefaultMaxBackoffDelay = 20 * time.Second
-//)
-//
-//func (r *DaxRetryer) setRetryerDefaults() {
-//	if r.BaseThrottleDelay == 0 {
-//		r.BaseThrottleDelay = DefaultBaseRetryDelay
-//	}
-//	if r.MaxBackoffDelay == 0 {
-//		r.MaxBackoffDelay = DefaultMaxBackoffDelay
-//	}
-//}
-//
-////RetryRules returns the delay duration before retrying this request again
-//func (r DaxRetryer) RetryRules(req *request.Request) time.Duration {
-//	// ??? ProvisionedThroughputExceededException
-//	if req.IsErrorThrottle() {
-//		r.setRetryerDefaults()
-//		attempt := req.RetryCount
-//		minDelay := time.Duration(1<<uint64(attempt)) * r.BaseThrottleDelay
-//		if minDelay > r.MaxBackoffDelay {
-//			minDelay = r.MaxBackoffDelay
-//		}
-//		jitter := time.Duration(rand.Intn(int(minDelay)/2 + 1))
-//
-//		return minDelay/2 + jitter
-//	}
-//	return 0
-//}
-//
-////ShouldRetry returns true if the request should be retried.
-//func (r DaxRetryer) ShouldRetry(req *request.Request) bool {
-//	daxErr := req.Error.(daxError)
-//	codes := daxErr.CodeSequence()
-//	return len(codes) > 0 && (codes[0] == 1 || codes[0] == 2) || req.IsErrorThrottle() || isAuthCRequiredException(codes)
-//}
-//
-//// Error code [4.23.31.33] is for AuthenticationRequiredException
-//func isAuthCRequiredException(codes []int) bool {
-//	return len(codes) == 4 && codes[0] == 4 && codes[1] == 23 && codes[2] == 31 && codes[3] == 33
-//}
-//
-//// MaxRetries returns the number of maximum retries the service will use to make
-//// an individual API request.
-//func (r DaxRetryer) MaxRetries() int {
-//	return 0
-//}
-
 // IsErrorRetryable returns if the error is daxError
 // if code sequences correct any condition return a value other than unknown.
 func IsErrorRetryable(err error) aws.Ternary {
